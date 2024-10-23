c8a87d66776cf1d1aaeb5bc72e49743e7f1ddee0
bergi <s.berke@netzkolchose.de>
2024-10-23 15:22:24 +0200

    BGJD-1135: do not zip label files in change-table mass-action when only one object selected

--- app/BotGard/tests/test_labels_svg.py
+++ app/BotGard/tests/test_labels_svg.py
@@ -1,6 +1,8 @@
 import re
 import subprocess
 import tempfile
+import zipfile
+import io
 from pathlib import Path
 from typing import List, Union
 
@@ -36,6 +38,38 @@ class TestLabelsSVG(TestCase):
         )
         self.assert_pdf_size(response.content, [252, 102])
 
+    def test_svg_label_multi(self):
+        self.assertTrue(
+            self.client.login(username="User1", password="the-secret"),
+            "failed to log in"
+        )
+
+        # --- multiple PDFS as zip from change-list action ---
+
+        response = self.get_label_response(
+            LabelDefinition.objects.get(id_name="A1"),
+            "garden",
+            [BotanicGarden.objects.get(name="Garden 1"), BotanicGarden.objects.get(name="Garden 2")],
+            "pdf",
+        )
+        self.assertEqual("application/zip", response.headers["Content-Type"])
+        fp = io.BytesIO(response.content)
+        with zipfile.ZipFile(fp) as zf:
+            self.assertEqual(2, len(zf.filelist))
+            for fileinfo in zf.filelist:
+                self.assertTrue(fileinfo.filename.endswith(".pdf"), fileinfo)
+                with zf.open(fileinfo.filename) as fp:
+                    self.assert_pdf_size(fp.read(), [252, 102])
+
+        # single selected file in change-list action returns no zip
+        response = self.get_label_response(
+            LabelDefinition.objects.get(id_name="A1"),
+            "garden",
+            [BotanicGarden.objects.get(name="Garden 1")],
+            "pdf",
+        )
+        self.assert_pdf_size(response.content, [252, 102])
+
     def assert_pdf_size(self, pdf_data: bytes, expected_size: List[int]):
         with tempfile.TemporaryDirectory() as path:
             filename = Path(path) / "label.pdf"
@@ -53,14 +87,24 @@ class TestLabelsSVG(TestCase):
                 url = reverse("admin:botman_botanicgarden_changelist")
             else:
                 url = reverse("admin:individuals_individual_changelist")
-            return self.client.post(
+            response = self.client.post(
                 url,
                 data={
-                    "action": f"label_{label_model.id_name}_{format}",
+                    "action": f"label_{label_model.id_name}",
                     "_selected_action": [str(o.pk) for o in object_model],
-                }
+                },
+                follow=True,
             )
         else:
-            return self.client.get(
+            response = self.client.get(
                 reverse(f"labels:{object_type}", args=(label_model.pk, object_model.pk)) + f"?format={format}",
             )
+
+        self.assertLess(response.status_code, 400)
+
+        err_msg = "Error creating labels"
+        if err_msg.encode() in response.content:
+            idx = response.content.find(err_msg.encode())
+            raise AssertionError(f"{err_msg}: {response.content[idx:idx + 5000]}")
+
+        return response
