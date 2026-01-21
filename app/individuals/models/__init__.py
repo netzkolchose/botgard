5b12f800a27a133426c5d9014b61c9b323647282
bergi <s.berke@netzkolchose.de>
2026-01-21 15:20:06 +0100

    reorder the imports of the individuals-app models to `dumpdata` them in the right order

--- app/individuals/models/__init__.py
+++ app/individuals/models/__init__.py
@@ -1,4 +1,4 @@
+from ..numbers import get_new_accession_number, get_new_order_number
+from .territory import Territory, TerritoryForm, Department, DepartmentForm
 from .individual import Individual, IndividualForm, Seed, SeedForm, SeedCatalog
 from .outplanting import Outplanting
-from .territory import Territory, TerritoryForm, Department, DepartmentForm
-from ..numbers import get_new_accession_number, get_new_order_number
