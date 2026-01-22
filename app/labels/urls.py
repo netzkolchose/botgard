a5893b08a46f79dc9ab6e552b5f3e13afa0e82ce
bergi <s.berke@netzkolchose.de>
2026-01-22 19:13:30 +0100

    BGJD-1146: add herbarium-species label type
    there is a bit of code duplication going on, need to fix this some day..

--- app/labels/urls.py
+++ app/labels/urls.py
@@ -8,11 +8,13 @@ urlpatterns = [
     re_path(r'^individual/(?P<label_pk>\d+)/(?P<indi_pk>\d+)/?$',           labels.label_individual, name='individual'),
     re_path(r'^garden/(?P<label_pk>\d+)/(?P<garden_pk>\d+)/?$',             labels.label_garden, name='garden'),
     re_path(r'^entry/(?P<label_pk>\d+)/(?P<entry_pk>\d+)/?$',               labels.label_entry, name='entry'),
+    re_path(r'^specimen/(?P<label_pk>\d+)/(?P<specimen_pk>\d+)/?$',         labels.label_herbarium_specimen, name='herbarium_specimen'),
 
     re_path(r'^random/(?P<label_pk>\d+)/?$',                                labels.label_random, name='random'),
     re_path(r'^random/individual/(?P<label_pk>\d+)/?$',                     labels.label_random_individual, name='random_individual'),
     re_path(r'^random/garden/(?P<label_pk>\d+)/?$',                         labels.label_random_garden, name='random_garden'),
     re_path(r'^random/entry/(?P<label_pk>\d+)/?$',                          labels.label_random_entry, name='random_entry'),
+    re_path(r'^random/specimen/(?P<label_pk>\d+)/?$',                       labels.label_herbarium_specimen, name='random_herbarium_specimen'),
 
     re_path(r'^docs/template/?$',                                           doc.label_template_doc, name='doc_template'),
 ]
