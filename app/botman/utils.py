e28769ef0d9a08655a948032443968ef3966e0da
bergi <s.berke@netzkolchose.de>
2026-07-13 23:56:32 +0200

    BGHA-1024: add utility func for fuzzy matching against BGCIGardens

--- app/botman/utils.py
+++ app/botman/utils.py
@@ -1,10 +1,13 @@
 import traceback
+from typing import List, Tuple, Union
+
+import rapidfuzz
 
 from django.db import transaction
 from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
-from botman.models import ExternalCatalog, ExternalCatalogArchive
+from botman.models import ExternalCatalog, ExternalCatalogArchive, BGCIGarden
 
 
 def move_catalogs_to_archive(admin, request, queryset):
@@ -35,3 +38,45 @@ def move_catalogs_to_archive(admin, request, queryset):
             level=messages.ERROR,
         )
         traceback.print_exc()
+
+
+def fuzzy_find_bgci_garden(
+        term: str,
+        max_num: int = 10,
+        keys: Tuple[str] = ("name", "city"),
+        with_scores: bool = False,
+) -> Union[List[BGCIGarden], List[Tuple[BGCIGarden, float]]]:
+    """
+    Find a BGCI garden by fuzzy-matching against `BGCIGarden.<key>`
+
+    The score for each key is summed and the highest `max_num` BGCIGarden instances are returned.
+    """
+    bgci_data = list(BGCIGarden.objects.all().values("pk", *keys))
+
+    scores = {}
+    for scorer in (
+            rapidfuzz.fuzz.ratio,
+            rapidfuzz.fuzz.partial_ratio,
+    ):
+        for key in keys:
+            for data, score in zip(
+                    bgci_data,
+                    rapidfuzz.process.cdist(
+                        [term.lower()],
+                        [(i[key] or "").lower() for i in bgci_data],
+                        scorer=scorer,
+                    )[0]
+            ):
+                scores[data["pk"]] = scores.get(data["pk"], 0) + score
+
+    pks_sorted = sorted(scores.keys(), key=lambda k: scores[k], reverse=True)
+
+    result = [
+        BGCIGarden.objects.get(pk=pk)
+        for pk in pks_sorted[:max_num]
+    ]
+
+    if with_scores:
+        result = [(g, float(scores[g.pk])) for g in result]
+
+    return result
